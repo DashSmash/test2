645763ffbc1fe91faad6b94748c0196180dd5bb8
Rishat\Ришат <alresh@bk.ru>
2017-06-12 14:11:51 +0500

    Create base structure

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: __dirname + '/public',
+        filename: "bundle.js"
+    }
+    // module: {
+    //     loaders: [
+    //         { test: /\.css$/, loader: "style!css" }
+    //     ]
+    // }
+}
